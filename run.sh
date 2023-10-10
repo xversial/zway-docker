173403e4e32543738edc8259e0b9649f26cec464
Brandon Xversial <xversial@vionox.com>
2023-10-10 03:47:01 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ docker container rm zway
 docker container run -d --restart unless-stopped -it \
  --mount source=zway-opt,target=/opt/z-way-server \
  --name=zway \
- --device /dev/ttyAMA0:/dev/ttyAMA0 \
+ --device /dev/ttyAMA0:/dev/ttyAMA0:rw \
  -p 8083:8083 \
  -p 8084:8084 \
  zway:latest
