02245815d88dd289adf1b1be7a7a794968c81f80
Brandon Xversial <xversial@vionox.com>
2023-10-10 01:14:26 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker container run --rm -d -it --mount source=zway-opt,target=/opt/z-way-server -p 8083:8083 --device=/dev/ttyAMA0 --name=zway zway:latest
+docker container stop zway 
+docker container rm zway 
+docker container run --rm -d -it --mount source=zway-opt,target=/opt/z-way-server -p 8083:8083 --device=/dev/ttyAMA0 --name=zway zway:latest 
